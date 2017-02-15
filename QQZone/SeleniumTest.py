3cebb6dd4c96981fe389b18514a1d62eb1c4d44c
DESKTOP-1VJF0IT\Maicius <maicius@outlook.com>
2017-02-16 00:31:43 +0800

    selenium的初步试验

--- QQZone/SeleniumTest.py
+++ QQZone/SeleniumTest.py
@@ -1,5 +1,13 @@
-# coding:utf-8
+import time
 from selenium import webdriver
 
-browser = webdriver.Chrome()
-browser.get('http://www.baidu.com')
+driver = webdriver.Chrome('E:/chromedriver/chromedriver')  # Optional argument, if not specified will search path.
+#driver.get('http://www.google.com/xhtml')
+driver.get('http://user.qzone.qq.com/1272082503')
+#driver.get('http://www.baidu.com')
+#time.sleep(5) # Let the user actually see something!
+search_box = driver.find_element_by_name('q')
+search_box.send_keys('QQ空间')
+search_box.submit()
+time.sleep(100)
+driver.quit()
