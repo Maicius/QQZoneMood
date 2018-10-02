50577ba24a9dac5295c79acb4cd07f00778af86c
Maicius <maicius@outlook.com>
2018-10-02 21:30:22 +0800

    添加点云图

--- QQZone/QQZoneAnalysis.py
+++ QQZone/QQZoneAnalysis.py
@@ -3,6 +3,12 @@ from QQZone import Spider
 import re
 import json
 import pandas as pd
+import jieba
+import time
+from wordcloud import WordCloud, ImageColorGenerator, STOPWORDS
+from scipy.misc import imread
+import codecs
+import matplotlib.pyplot as plt
 
 
 class QQZoneAnalysis(Spider):
@@ -112,6 +118,43 @@ class QQZoneAnalysis(Spider):
             gender = uin['gender']
             uin_list.append(dict(nick=nick, gender=gender))
         return total_num, uin_list
+
+    def drawWordCloud(word_text, filename):
+        mask = imread('bike.jpg')
+        my_wordcloud = WordCloud(
+            background_color='white',  # 设置背景颜色
+            mask=mask,  # 设置背景图片
+            max_words=2000,  # 设置最大现实的字数
+            stopwords=STOPWORDS,  # 设置停用词
+            font_path='/System/Library/Fonts/Hiragino Sans GB W6.ttc',  # 设置字体格式，如不设置显示不了中文
+            max_font_size=50,  # 设置字体最大值
+            random_state=30,  # 设置有多少种随机生成状态，即有多少种配色方案
+            scale=1.3
+        ).generate(word_text)
+        image_colors = ImageColorGenerator(mask)
+        my_wordcloud.recolor(color_func=image_colors)
+        # 以下代码显示图片
+        plt.imshow(my_wordcloud)
+        plt.axis("off")
+        plt.show()
+        # 保存图片
+        my_wordcloud.to_file(filename=filename)
+
+    def get_jieba_words(content):
+        word_list = jieba.cut(content, cut_all=False)
+        word_list2 = []
+        waste_words = "现在 时候 这里 那里 今天 明天 非常 出去 各种 其实 真是 有点 只能 有些 只能 小时 baidu 还好 回到 好多 好的 继续 不会 起来 虽然 然饿 幸好一个 一些 一下 一样 一堆 所有 这样 那样 之后 只是 每次 所以 为了 还有 这么 那么 个人 因为 每次 但是 不想 出来 的话 这种 那种 开始 觉得 这个 那个 几乎 最后 自己 这些 那些 总之 " \
+                      "有没有 没有 并且 然后 随便 可以 太大 应该 uin nick  真的 真好 可以 不要是不是 真的或者 可以之前 不能突然最近颇极十分就都马上立刻曾经居然重新" \
+                      "不断已已经曾曾经刚刚正在将要、就、就要、马上、立刻、顿时、终于、常、常常、时常、时时、往往、渐渐、早晚、从来、终于、一向、向来、从来、总是、始终、" \
+                      "水、赶紧、仍然、还是、屡次、依然、重新、还、再、再三、偶尔都、总、共、总共、统统、只、仅仅、单、净、光、一齐、一概、一律、单单、就大肆、肆意、特意、" \
+                      "亲自、猛然、忽然、公然、连忙、赶紧、悄悄、暗暗、大力、稳步、阔步、单独、亲自难道、岂、究竟、偏偏、索性、简直、就、可、也许、难怪、大约、幸而、幸亏、" \
+                      "反倒、反正、果然、居然、竟然、何尝、何必、明明、恰恰、未免、只好、不妨"
+        for word in word_list:
+            print(word)
+            if len(word) >= 2 and word.find('e') == -1 and waste_words.find(word) == -1:
+                word_list2.append(word)
+        words_text = " ".join(word_list2)
+        return words_text
 
 
 if __name__ == '__main__':
