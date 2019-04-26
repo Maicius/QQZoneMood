7dee241555ade6479ad64f42b44083c9bd45ba58
maicius <maicius@outlook.com>
2019-04-27 03:55:34 +0800

    双向绑定

--- src/web/static/script.js
+++ src/web/static/script.js
@@ -1,11 +1,18 @@
-let history_dom = echarts.init(document.getElementById(HISTORY_DOM));
+// let history_dom = echarts.init(document.getElementById(HISTORY_DOM));
 // avalon.config({
 //     interpolate: ["[[","]]"]
 // });
-
+let history_dom;
 let vm = avalon.define({
     $id: 'qqzone',
-
+    nick_name: '',
+    qq_id: '',
+    stop_num: -1,
+    stop_date: '-1',
+    no_delete: false,
+    agree: false,
+    disable: 'false',
+    qq_cookie: '',
     fetch_history_data: function () {
         $.ajax({
             url: '/get_history/1272082503/maicius',
@@ -14,10 +21,39 @@ let vm = avalon.define({
                 draw_history_line(history_dom, result, "QQ空间说说历史曲线图");
             }
         })
+    },
+
+    submit_data: function () {
+        if (agree) {
+            $.ajax({
+                url: "/test",
+                data: {
+                    nick_name: vm.nick_name,
+                    qq: vm.qq_id,
+                    stop_date: vm.stop_date,
+                    stop_num: vm.stop_num
+                },
+                success: function (data) {
+                    console.log(data);
+                }
+            })
+        }
+    },
+    clear_cache: function () {
+        
     }
 });
 
-
+vm.$watch("agree", function (new_v, old_v) {
+    switch (new_v) {
+        case true:
+            $('#start_get').removeAttr("disabled");
+            break;
+        case false:
+            $('#start_get').attr("disabled", "true");
+            break;
+    }
+});
 
 $(document).ready(function () {
     //vm.fetch_history_data();
