97d64a1fde6332f77f051c411751bdf96690b80b
maicius <maicius@outlook.com>
2019-05-15 15:12:21 +0800

    增加测试用例

--- test/spider/spider_test.py
+++ test/spider/spider_test.py
@@ -2,9 +2,13 @@ import unittest
 
 from src.spider.BaseSpider import BaseSpider
 from src.spider.QQZoneSpider import QQZoneSpider
+from src.spider.main import capture_main_data
 
 
 class SpiderTest(unittest.TestCase):
+    """
+    对Spider进行测试
+    """
 
     def test_connect_redis(self):
         bs = BaseSpider(use_redis=True, from_web=True)
@@ -46,3 +50,32 @@ class SpiderTest(unittest.TestCase):
                           download_small_image=True)
         sp.login()
         sp.get_mood_list()
+
+    # 测试抽取tid
+    def test_extract_tid(self):
+        sp = QQZoneSpider(use_redis=False, debug=False, mood_num=20, download_mood_detail=False,
+                          download_like_detail=True, download_like_names=False)
+        str1 = 'http://user.qzone.qq.com/1272082503/mood/4770d24b00acd95c13e00500.1<.>http://user.qzone.qq.com/1272082503/mood/4770d24b00acd95c13e00500.1'
+        str2 = 'http://user.qzone.qq.com/1272082503/mood/4770d24bbb83d95c05600900.1^||^https://b339.photo.store.qq.com/psb?/91b7c939-e3c7-41d8-a450-626a11d7ce19/4iKMKW.M8phXT8bghzPgKjz.rdkmby5vOiOJi4PqSlk!/b/dFMBAAAAAAAA&bo=7gI2Be4CNgURECc!^||^0'
+        str3 = 'http://user.qzone.qq.com/1272082503/mood/4770d24b5c91d65cf9af0b00.1<.>http://user.qzone.qq.com/1272082503/mood/4770d24b5c91d65cf9af0b00.1'
+        expect1 = '4770d24b00acd95c13e00500'
+        expect2 = '4770d24bbb83d95c05600900'
+        expect3 = '4770d24b5c91d65cf9af0b00'
+
+        assert expect1 == sp.extract_tid_from_unikey(str1)
+        assert expect2 == sp.extract_tid_from_unikey(str2)
+        assert expect3 == sp.extract_tid_from_unikey(str3)
+
+    def test_capture_info(self):
+        sp = QQZoneSpider(use_redis=True, debug=False, mood_num=20, download_mood_detail=False,
+                          download_like_detail=True, download_like_names=False)
+
+        sp.login()
+        sp.get_mood_list()
+
+    #
+    def test_capture_info_parallel(self):
+        sp = QQZoneSpider(use_redis=True, debug=False, mood_num=200)
+        sp.login()
+        sp.get_mood_list()
+
