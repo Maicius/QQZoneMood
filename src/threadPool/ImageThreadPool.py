198594bac71524d9c264db3ea2c6da05012ccb61
maicius <maicius@outlook.com>
2019-05-16 09:36:43 +0800

    统计线程池在get_thread时等待的时间

--- src/threadPool/ImageThreadPool.py
+++ src/threadPool/ImageThreadPool.py
@@ -4,7 +4,6 @@ import time
 class ImageThreadPool(object):
     def __init__(self, max_thread=20):
         self.queue = []
-        self.count = 0
         self.time_spend = 0
         for i in range(max_thread):
             self.queue.append(threading.Thread)
@@ -15,7 +14,6 @@ class ImageThreadPool(object):
             length = len(self.queue)
             if length > 0:
                 spend = int(time.time() * 1000 - begin_time)
-                self.count += 1
                 self.time_spend += spend
                 return self.queue.pop(0)
 
