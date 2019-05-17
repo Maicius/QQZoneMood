008f0428e4e98085988b4e5cbd4efd23b1f00b04
maicius <maicius@outlook.com>
2019-05-17 17:36:22 +0800

    添加统一入口

---
+++ test/__init__.py
@@ -0,0 +1,13 @@
+import unittest
+
+from test.spider.friend_mood_spider_test import FriendMoodSpiderTest
+from test.spider.friend_spider_test import FriendSpiderTest
+from test.spider.spider_test import SpiderTest
+from test.analysis.analysis_test import AnalysisTest
+
+if __name__ == '__main__':
+    # SpiderTest()
+    # FriendSpiderTest()
+    # FriendMoodSpiderTest()
+    AnalysisTest()
+    unittest.main()
