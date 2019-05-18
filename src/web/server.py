469c744e9606804fcf8d865b967a4be3646e1eba
maicius <maicius@outlook.com>
2019-05-18 19:10:09 +0800

    修改提示

--- src/web/server.py
+++ src/web/server.py
@@ -5,8 +5,8 @@ from datetime import timedelta
 curPath = os.path.abspath(os.path.dirname(__file__))
 rootPath = os.path.split(curPath)[0]
 sys.path.append(os.path.split(rootPath)[0])
-from src.web.web_util.web_util import judge_pool
-
+from src.web.web_util.web_util import judge_pool, get_redis_conn, WAITING_USER_LIST
+import redis
 from flask import Flask, render_template,session
 from src.web.controller.dataController import data, POOL_FLAG
 from src.web.controller.spiderController import spider
@@ -14,6 +14,9 @@ from src.web.controller.spiderController import spider
 app = Flask(__name__)
 app.config['SECRET_KEY']=os.urandom(24)
 app.config['PERMANENT_SESSION_LIFETIME']=timedelta(days=7)
+host = judge_pool()
+conn = get_redis_conn(host)
+conn.delete(WAITING_USER_LIST)
 @app.route('/')
 def config():
     session[POOL_FLAG] = judge_pool()
